753a6bb75060f19240ab02213b39aade4af5aea3
MrQureshi <30835780+MrQureshi@users.noreply.github.com>
2018-05-09 19:39:02 +0500

    todo

---
+++ reactnotes/src/Config/config.js
@@ -0,0 +1,10 @@
+
+  // Initialize Firebase
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyCP4A-7BTwEKCnn1s0-aCxc1zxiucmRXmQ",
+    authDomain: "todolist-00000.firebaseapp.com",
+    databaseURL: "https://todolist-00000.firebaseio.com",
+    projectId: "todolist-00000",
+    storageBucket: "todolist-00000.appspot.com",
+    messagingSenderId: "43017782973"
+  };
